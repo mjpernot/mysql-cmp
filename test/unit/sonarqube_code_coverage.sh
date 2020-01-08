e1597071df69cbdea817e69d980df5be8c84b028
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-01-08 14:18:55 +0000

    Update unit test runs

--- test/unit/sonarqube_code_coverage.sh
+++ test/unit/sonarqube_code_coverage.sh
@@ -12,6 +12,7 @@ coverage run -a --source=mysql_rep_cmp test/unit/mysql_rep_cmp/fetch_db_list.py
 coverage run -a --source=mysql_rep_cmp test/unit/mysql_rep_cmp/help_message.py
 coverage run -a --source=mysql_rep_cmp test/unit/mysql_rep_cmp/recur_tbl_cmp.py
 coverage run -a --source=mysql_rep_cmp test/unit/mysql_rep_cmp/run_cmp.py
+coverage run -a --source=mysql_rep_cmp test/unit/mysql_rep_cmp/setup_cmp.py
 
 echo ""
 echo "Producing code coverage report"
