9fade34bb95e31cd0e499faad482c82df61e48c0
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-01-09 15:25:43 +0000

    Update test/unit/mysql_rep_cmp/main.py

--- test/unit/mysql_rep_cmp/main.py
+++ test/unit/mysql_rep_cmp/main.py
@@ -43,6 +43,9 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_programlock_id -> 
+        test_programlock_false -> Test with ProgramLock returns False.
+        test_programlock_true -> Test with ProgramLock returns True.
         test_arg_dir_chk_crt_false -> Test arg_dir_chk_crt if returns false.
         test_arg_dir_chk_crt_true -> Test arg_dir_chk_crt if returns true.
         test_arg_cond_req_true -> Test arg_cond_req if returns true.
@@ -67,6 +70,83 @@ class UnitTest(unittest.TestCase):
         """
 
         self.args_array = {"-c": "CfgFile", "-d": "CfgDir"}
+
+    @mock.patch("mysql_rep_cmp.gen_class.ProgramLock",
+                mock.Mock(side_effect=None))
+    @mock.patch("mysql_rep_cmp.run_program", mock.Mock(return_value=True))
+    @mock.patch("mysql_rep_cmp.gen_libs.help_func")
+    @mock.patch("mysql_rep_cmp.arg_parser")
+    def test_programlock_id(self, mock_arg, mock_help):
+
+        """Function:  test_programlock_id
+
+        Description:  Test with ProgramLock with flavor id.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_help.return_value = False
+        mock_arg.arg_req_xor.return_value = True
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_cond_req.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+
+        self.args_array["-y"] = "FlavorID"
+
+        self.assertFalse(mysql_rep_cmp.main())
+
+    @mock.patch(
+        "mysql_rep_cmp.gen_class.ProgramLock",
+        mock.Mock(
+            side_effect=mysql_rep_cmp.gen_class.SingleInstanceException))
+    @mock.patch("mysql_rep_cmp.run_program", mock.Mock(return_value=True))
+    @mock.patch("mysql_rep_cmp.gen_libs.help_func")
+    @mock.patch("mysql_rep_cmp.arg_parser")
+    def test_programlock_false(self, mock_arg, mock_help):
+
+        """Function:  test_programlock_false
+
+        Description:  Test with ProgramLock returns False.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_help.return_value = False
+        mock_arg.arg_req_xor.return_value = True
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_cond_req.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_rep_cmp.main())
+
+    @mock.patch("mysql_rep_cmp.gen_class.ProgramLock",
+                mock.Mock(side_effect=None))
+    @mock.patch("mysql_rep_cmp.run_program", mock.Mock(return_value=True))
+    @mock.patch("mysql_rep_cmp.gen_libs.help_func")
+    @mock.patch("mysql_rep_cmp.arg_parser")
+    def test_programlock_true(self, mock_arg, mock_help):
+
+        """Function:  test_programlock_true
+
+        Description:  Test with ProgramLock returns True.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_help.return_value = False
+        mock_arg.arg_req_xor.return_value = True
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_cond_req.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+
+        self.assertFalse(mysql_rep_cmp.main())
 
     @mock.patch("mysql_rep_cmp.run_program", mock.Mock(return_value=True))
     @mock.patch("mysql_rep_cmp.gen_libs.help_func")
