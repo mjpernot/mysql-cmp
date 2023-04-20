732ca7292be9d4ebcb7e24a65c9872f19dc554f1
Mark Pernot <mark.pernot@gmail.com>
2023-04-20 12:50:44 +0100

    Merge of mod/332c1 into mod/332

--- test/unit/mysql_rep_cmp/unit_test_run.sh
+++ test/unit/mysql_rep_cmp/unit_test_run.sh
@@ -6,10 +6,10 @@
 
 echo ""
 echo "Unit testing..."
-test/unit/mysql_rep_cmp/fetch_db_list.py
-test/unit/mysql_rep_cmp/help_message.py
-test/unit/mysql_rep_cmp/main.py
-test/unit/mysql_rep_cmp/recur_tbl_cmp.py
-test/unit/mysql_rep_cmp/run_cmp.py
-test/unit/mysql_rep_cmp/run_program.py
-test/unit/mysql_rep_cmp/setup_cmp.py
+/usr/bin/python test/unit/mysql_rep_cmp/fetch_db_list.py
+/usr/bin/python test/unit/mysql_rep_cmp/help_message.py
+/usr/bin/python test/unit/mysql_rep_cmp/main.py
+/usr/bin/python test/unit/mysql_rep_cmp/recur_tbl_cmp.py
+/usr/bin/python test/unit/mysql_rep_cmp/run_cmp.py
+/usr/bin/python test/unit/mysql_rep_cmp/run_program.py
+/usr/bin/python test/unit/mysql_rep_cmp/setup_cmp.py
---
+++ test/unit/mysql_rep_cmp/unit_test_run3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Unit testing program for the program module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file
+#   is located at.
+
+echo ""
+echo "Unit testing..."
+/usr/bin/python3 test/unit/mysql_rep_cmp/fetch_db_list.py
+/usr/bin/python3 test/unit/mysql_rep_cmp/help_message.py
+/usr/bin/python3 test/unit/mysql_rep_cmp/main.py
+/usr/bin/python3 test/unit/mysql_rep_cmp/recur_tbl_cmp.py
+/usr/bin/python3 test/unit/mysql_rep_cmp/run_cmp.py
+/usr/bin/python3 test/unit/mysql_rep_cmp/run_program.py
+/usr/bin/python3 test/unit/mysql_rep_cmp/setup_cmp.py
